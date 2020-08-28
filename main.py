238f82223c081d51f7baaba6a79af883ad65fe03
sytk <33809675+sytk@users.noreply.github.com>
2020-08-29 02:14:35 +0900

    Revert "ProcessPoolExecutorに変更。if __name__ == '__main__'を追加"
    This reverts commit 99be113fb340f47a651fe362e5f5deb2ca30bf49.

--- main.py
+++ main.py
@@ -8,52 +8,50 @@ import numpy as np
 import time
 from hand_ges import HandGesture
 import concurrent.futures
-if __name__ == '__main__':
 
-    # source ./venv/bin/activate
-    WINDOW = "Hand Tracking"
+WINDOW = "Hand Tracking"
 
 
-    cv2.namedWindow(WINDOW)
-    capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    # capture.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
-    # capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cv2.namedWindow(WINDOW)
+capture = cv2.VideoCapture(0)
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 960)
+# capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
-    if capture.isOpened():
-        hasFrame, frame = capture.read()
-    else:
-        hasFrame = False
+if capture.isOpened():
+    hasFrame, frame = capture.read()
+else:
+    hasFrame = False
 
-    # fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    # out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+# fourcc = cv2.VideoWriter_fourcc(*'XVID')
+# out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
-    detector = HandGesture()
-    executor = concurrent.futures.ProcessPoolExecutor(max_workers=2)
+detector = HandGesture()
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
 
-    while hasFrame:
-        start = time.time()
+while hasFrame:
+    start = time.time()
 
-        hasFrame, frame = capture.read()
-        frame = cv2.flip(frame, 1)
+    hasFrame, frame = capture.read()
+    frame = cv2.flip(frame, 1)
 
-        if executor.submit(cv2.imshow, WINDOW, frame).done():
-            executor.submit(cv2.imshow, WINDOW, frame)
-        if executor.submit(detector.updateGesture, frame).done():
-            executor.submit(detector.updateGesture, frame)
+    if executor.submit(cv2.imshow, WINDOW, frame).done():
+        executor.submit(cv2.imshow, WINDOW, frame)
+    if executor.submit(detector.updateGesture, frame).done():
+        executor.submit(detector.updateGesture, frame)
 
-        ges = detector.getGesture()
-        palm = detector.getPalmPos()
-        depth = detector.getPalmDepth()
-        print(ges, palm, depth)
+    ges = detector.getGesture()
+    palm = detector.getPalmPos()
+    depth = detector.getPalmDepth()
+    print(ges, palm, depth)
 
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-        print(1 / (time.time() - start))
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    print(1 / (time.time() - start))
 
-    capture.release()
-    executor.shutdown()
-    # out.release()
-    cv2.destroyAllWindows()
+capture.release()
+executor.shutdown()
+# out.release()
+cv2.destroyAllWindows()
